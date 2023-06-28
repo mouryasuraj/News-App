818a3d7bd76bd0c5a6b863aac2e62644f9766c0d
Suraj Mourya <2276661@gnkhalsa.edu.in>
2023-06-28 15:17:34 +0530

    added infinte scroll using npm react infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Loading from './Loading';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 export class News extends Component {
 
   // Deafault Props
@@ -31,15 +32,14 @@ export class News extends Component {
       articles:[],
       loading:false,
       page:1,
+      totalResults:0
     }
+    console.log(this.state.articles.length)
     document.title = `${this.capitalize(this.props.category)} - NewsWave`
   }
 
-
-
-  // ComponenetDidMount function 
-  async componentDidMount(){
-    let sportUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2dac573a34394642a7927bef0d45bd54&page=1&pageSize=${this.props.pageSize}`
+  updateNews = async() =>{
+    const sportUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2dac573a34394642a7927bef0d45bd54&page=${this.state.page}&pageSize=${this.props.pageSize}`
     this.setState({loading:true})
     let data = await fetch(sportUrl);
     let parsedData =await data.json();
@@ -48,36 +48,31 @@ export class News extends Component {
       totalResults : parsedData.totalResults,
       loading:false
     })
-    console.log(parsedData)
   }
 
-  // Next Button function
-  handleNxtClick = async() => {
-    
-      let sportUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2dac573a34394642a7927bef0d45bd54&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-      this.setState({loading:true})
-      let data = await fetch(sportUrl);
-      let parsedData =await data.json();
-      this.setState({
-        articles : parsedData.articles,
-        page : this.state.page + 1,
-        loading:false
-      })
-      
-  }
-
-  // Previous Button function
-  handlePrevClick = async() => {
-    let sportUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2dac573a34394642a7927bef0d45bd54&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
-    this.setState({loading:true})
+  // Fetch More Data
+  fetchMoreData = async() => {
+    this.setState({
+      page:this.state.page + 1
+    })
+    const sportUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2dac573a34394642a7927bef0d45bd54&page=${this.state.page}&pageSize=${this.props.pageSize}`
     let data = await fetch(sportUrl);
     let parsedData =await data.json();
     this.setState({
-      articles : parsedData.articles,
-      page : this.state.page - 1,
-      loading:false
+      articles : this.state.articles.concat(parsedData.articles),
+      totalResults : parsedData.totalResults
     })
+  };
+
+
+
+  // ComponenetDidMount function 
+  async componentDidMount(){
+    this.updateNews()
   }
+
+
+ 
 
 
   render() {
@@ -88,21 +83,24 @@ export class News extends Component {
       {/* Heading */}
         <h2 className='md:text-3xl  sm:text-xl text-xl text-center font-bold text-slate-900 rounded-sm p-4 mb-5 '>NewsWave - Top {this.capitalize(this.props.category)} Headlines</h2>
         {/* News */}
-      {this.state.loading && <Loading />}
-        <div className='flex flex-wrap gap-10 justify-center xl:px-44'>
-        {!(this.state.loading) && this.state.articles.map((e)=>{
-          return(
-
-              <NewsItem key={e.url} title={e.title} description={e.description ? e.description.slice(0,180):"Click read more for description"} imageUrl={e.urlToImage} newsUrl={e.url} source={e.source.name} publishedAt={e.publishedAt} author={e.author} />
-          )
-          
-        })}
-        </div>
-        {/* Next and Previous Button */}
-        <div className='mt-[50px] md:px-44 flex justify-between'>
-            <button disabled={this.state.page <=1} className='bg-slate-700 text-lg border-[1px] p-2 text-white ' onClick={this.handlePrevClick}>&larr; Previous</button>
-            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className='bg-slate-700 text-lg border-[1px] p-2 text-white'  onClick={this.handleNxtClick}>Next &rarr;</button>
-        </div>
+      {/* {this.state.loading && <Loading />} */}
+      <InfiniteScroll
+          key={this.state.articles.url}
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Loading />}
+        >
+          <div className='flex flex-wrap gap-10 justify-center xl:px-44'>
+              {this.state.articles.map((e)=>{
+                return(
+                    <NewsItem key={e.url} title={e.title} description={e.description ? e.description.slice(0,180):"Click read more for description"} imageUrl={e.urlToImage} newsUrl={e.url} source={e.source.name} publishedAt={e.publishedAt} author={e.author} />
+                )
+                
+              })}
+          </div>
+        </InfiniteScroll>
+        
         
       </div>
       </>
